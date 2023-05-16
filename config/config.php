d73d50150f3ef0be81e05985d7bbe1dc0c5fa19d
Diego <jdcasee21@gmail.com>
2023-05-16 10:25:19 -0600

    Prueba

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('URL', 'http://localhost/expense-app-master/');
+
+define('HOST', 'localhost');
+define('DB', 'servicio_escolar');
+define('USER', 'root');
+define('PASSWORD', "1234");
+define('CHARSET', 'utf8mb4');
+
+?>
